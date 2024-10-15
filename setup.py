01bf71b22ce8f4dd339bee51f16c3b1121b8c70c
shaheennabi <ishaheennabi333@gmail.com>
2024-10-15 03:06:17 -0700

    setup run done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Shaheen Nabi",
+    author_email="ishaheenanbi333@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
