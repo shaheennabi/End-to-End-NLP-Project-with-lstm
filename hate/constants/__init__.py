9e18de918dd856ccfd4e0c6a727506d1537fb399
shaheennabi <ishaheennabi333@gmail.com>
2024-10-16 23:11:22 -0700

    data ingestion constant added

--- hate/constants/__init__.py
+++ hate/constants/__init__.py
@@ -5,7 +5,17 @@ from datetime import datetime
 # Common constants
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
-BUCKET_NAME = "hate-speech"
+BUCKET_NAME = "hate-speech-classification"
 ZIP_FILE_NAME = 'dataset.zip'
 LABEL = 'label'
 TWEET = 'tweet'
+
+
+
+"""
+Data Ingestion related constants
+"""
+
+DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
+DATA_INGESTION_IMBALANCE_DATA_DIR = "imbalanced_data.csv"
+DATA_INGESTION_RAW_DATA_DIR  = "raw_data.csv"
