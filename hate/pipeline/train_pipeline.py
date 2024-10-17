8dd0e3ef5118b71935a4e61e5dc894e67d501be5
shaheennabi <ishaheennabi333@gmail.com>
2024-10-17 09:15:47 -0700

    train pipeline updated for validation

--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -1,37 +1,53 @@
-import  sys
+import sys
 from hate.logger import logging
 from hate.exception import HateException
 from hate.components.data_ingestion import DataIngestion
 from hate.entity.config_entity import DataIngestionConfig
 from hate.entity.artifact_entity import DataIngestionArtifact
+from hate.components.data_validation import DataValidation
+from hate.entity.config_entity import DataValidationConfig
+from hate.entity.artifact_entity import DataValidationArtifact
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        
-
-
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         logging.info("Entered the start_data_ingestion method of TrainPipeline class")
-        try: 
+        try:
             logging.info("Getting the data from S3Bucket")
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
             logging.info("Got the imbalanced and raw csv's from S3 bucket")
             logging.info("Exited the start_data_ingestion method of TrainPipeline class")
             return data_ingestion_artifact
-        
-        except Exception as e: 
+
+        except Exception as e:
             raise HateException(e, sys)
-        
+
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        logging.info("Entering the start_data_validation method of TrainPipeline class")
+        try:
+            data_validation = DataValidation(
+    data_ingestion_artifact=data_ingestion_artifact,  
+    data_validation_config=self.data_validation_config
+)
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+            return data_validation_artifact
+
+        except Exception as e:
+            raise HateException(e, sys)
 
     def run_pipeline(self):
-        logging.info("Entered the run_pipeline method TrainPipeline class")
-        try: 
+        logging.info("Entered the run_pipeline method of TrainPipeline class")
+        try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
 
             logging.info("Exited the run_pipeline method of TrainPipeline class")
-        except Exception as e: 
-            raise HateException(e,sys)
+        except Exception as e:
+            raise HateException(e, sys)
