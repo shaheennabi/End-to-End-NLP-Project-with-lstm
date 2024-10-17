a585fc5f6a5bd065f81cdbdcba9acf9968c20091
shaheennabi <ishaheennabi333@gmail.com>
2024-10-17 00:17:13 -0700

    data ingestion pipeline added

---
+++ hate/pipeline/train_pipeline.py
@@ -0,0 +1,28 @@
+import  sys
+from hate.logger import logging
+from hate.exception import HateException
+from hate.components.data_ingestion import DataIngestion
+from hate.entity.config_entity import DataIngestionConfig
+from hate.entity.artifact_entity import DataIngestionArtifact
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        
+
+
+
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+        try: 
+            logging.info("Getting the data from S3Bucket")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Got the imbalanced and raw csv's from S3 bucket")
+            logging.info("Exited the start_data_ingestion method of TrainPipeline class")
+            return data_ingestion_artifact
+        
+        except Exception as e: 
+            raise HateException(e, sys)
+        
